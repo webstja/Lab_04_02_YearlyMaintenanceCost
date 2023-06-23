a85ecb9b2ea9e9a634875caa00343d74a76222be
webstja <136018686+webstja@users.noreply.github.com>
2023-06-23 12:43:43 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    int springMaintenance = 627;
+    int summerMaintenance = 476;
+    int fallMaintenance = 525;
+    int winterMaintenance = 638;
+    int totalMaintenance = springMaintenance + summerMaintenance + fallMaintenance + winterMaintenance;
+    System.out.println("Your Spring home maintenance cost is $" + springMaintenance + ". " + "Your Summer home maintenance cost is $" + summerMaintenance + ". " + "Your Fall home maintenance cost is $" + fallMaintenance + ". " + "Your winter home maintenance cost is $" + winterMaintenance + ". " + "Your total yearly maintenance cost is $" + totalMaintenance + ".");
+    }
+}
